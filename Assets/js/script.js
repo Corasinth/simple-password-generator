2db997c457030c6773b1730a3e9564097608f02e
Corasinth <beforeyouknowkindness@gmail.com>
2022-06-24 17:43:30 -0500

    add arrays

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,3 +1,23 @@
+// Arrays
+var lowercaseLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+var uppercaseLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+var special = ['"', "!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]","|", "}", "~", "^", "_" , "`", "{",  "\\",]
+
+var selectorArray = []
+var passwordArray = []
+
+//Variables
+
+
+//Functions
+
+
+function generatePassword () {
+  var num1 = 1+1
+  return num1
+}
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
