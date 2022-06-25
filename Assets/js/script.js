adfe84e78ebba69353b51fd925abe94ec2f522c0
Corasinth <beforeyouknowkindness@gmail.com>
2022-06-24 19:59:18 -0500

    add semicolons

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,32 +1,32 @@
 // Arrays
-var lowercaseLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-var uppercaseLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
-var special = ['"', "!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]","|", "}", "~", "^", "_" , "`", "{",  "\\",]
+var lowercaseLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var uppercaseLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var special = ['"', "!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]","|", "}", "~", "^", "_" , "`", "{",  "\\",];
 
-var selectorArray = []
-var passwordArray = []
+var selectorArray = [];
+var passwordArray = [];
 
 //Other Variables
-var confirmUppercase 
-var confirmLowercase 
-var confirmNumbers 
-var confirmSpecial
-var numOfCharacters = 0
-var password
+var confirmUppercase;
+var confirmLowercase;
+var confirmNumbers;
+var confirmSpecial;
+var numOfCharacters = 0;
+var password;
 
 //Functions
 
 //Create confirmation boxes and error message if user selects no choices
 function popConfirms () {
-  confirmUppercase = window.confirm ("Should the password include uppercase letters?")
-  confirmLowercase = window.confirm ("Should the password include lowercase letters?")
-  confirmNumbers = window.confirm ("Should the password include numbers?")
-  confirmSpecial = window.confirm ("Should the password include symbols?")
+  confirmUppercase = window.confirm ("Should the password include uppercase letters?");
+  confirmLowercase = window.confirm ("Should the password include lowercase letters?");
+  confirmNumbers = window.confirm ("Should the password include numbers?");
+  confirmSpecial = window.confirm ("Should the password include symbols?");
   // Error message logic if nothing is chosen
   if (confirmUppercase === false && confirmLowercase === false && confirmNumbers === false && confirmSpecial === false) { 
-    window.alert ("Please select at least one character type.")
-    popConfirms ()
+    window.alert ("Please select at least one character type.");
+    popConfirms ();
   }
 }
 //Creates selectorArray based on user's choices
@@ -86,9 +86,9 @@ var generateBtn = document.querySelector("#generate");
 // Write password to the #password input
 function writePassword() {
   // Various confirm dialogues assigning their True/False outputs to variables 
-  popConfirms ()
-  createSelectorArray ()
-  promptNumber ()
+  popConfirms ();
+  createSelectorArray ();
+  promptNumber ();
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
   passwordText.value = password;
